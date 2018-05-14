a0fe62a944151b364e5f8d2e79dd747266cd56f4
tenisistaa <krzychu_ten@o2.pl>
2018-05-14 22:57:25 +0200

    my-button

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+      <div className="row">
+              <div className="col-sm-12 text-center">
+                <h3>Zestawienie wyników wtorkowego grania</h3>
+              </div>
+            </div>
         <div className="row">
           <div className="col-sm-10 bg-success">
-
+            
             <div className="row">
-              <div className="col-sm-5 border text-center bg-warning">
+              <div className="col-sm-1"></div>
+              <div className="col-sm-4 border text-center bg-warning">
               Mateusz
               </div>
               <div className="col-sm-1 border text-center bg-secondary text-light">
@@ -20,13 +26,15 @@ class App extends Component {
               <div className="col-sm-1 border text-center bg-secondary text-light">
               3
               </div>
-              <div className="col-sm-5 border text-center bg-primary">
+              <div className="col-sm-4 border text-center bg-primary">
               Michał, Paweł, Jacek
               </div>
+              <div className="col-sm-1"></div>
             </div>
 
             <div className="row">
-              <div className="col-sm-5 border text-center bg-light">
+              <div className="col-sm-1"></div>
+              <div className="col-sm-4 border text-center bg-light">
               Karol, Janek
               </div>
               <div className="col-sm-1 border text-center bg-secondary text-light">
@@ -35,34 +43,57 @@ class App extends Component {
               <div className="col-sm-1 border text-center bg-secondary text-light">
               2
               </div>
-              <div className="col-sm-5 border text-center bg-primary">
+              <div className="col-sm-4 border text-center bg-primary">
               Leon, Jacek
               </div>
+              <div className="col-sm-1"></div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-1"></div>
+              <div className="col-sm-4 border text-center bg-light">
+              Karol, Piotrek, Karol, Alan
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              4
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              3
+              </div>
+              <div className="col-sm-4 border text-center bg-warning">
+              Mateusz, Wojtek, Jan
+              </div>
+              <div className="col-sm-1"></div>
             </div>
 
 
           </div>
 
-          <div className="col-sm-2 border">
+          <div className="col-sm-2 ">
             <div className="row">
-              <div className="col-sm-12 border">
-                <button type="button" className="btn btn-info">
+              <div className="col-sm-12">
+                <button type="button" className="my-button btn-info">
                 Tabela
                 </button>
               </div>
-              <div className="col-sm-12 border">
-                <button type="button" className="btn btn-info">
+              <div className="col-sm-12">
+                <button type="button" className="my-button btn-info">
                 Strzelcy
                 </button>
               </div>
-              <div className="col-sm-12 border">
-                <button type="button" className="btn btn-info">
+              <div className="col-sm-12">
+                <button type="button" className="my-button btn-info">
                 Sala 1
                 </button>
               </div>
-              <div className="col-sm-12 border">
-                <button type="button" className="btn btn-info">
+              <div className="col-sm-12">
+                <button type="button" className="my-button btn-info">
                 Sala 2
+                </button>
+              </div>
+              <div className="col-sm-12">
+                <button type="button" className="my-button btn-info">
+                Ustawienia
                 </button>
               </div>
             </div>
