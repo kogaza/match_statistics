c38d46d48ef9834e7df1f6434742e19eb162ba2c
tenisistaa <krzychu_ten@o2.pl>
2018-05-14 22:02:18 +0200

    bootstrap 2

--- src/App.js
+++ src/App.js
@@ -6,14 +6,47 @@ import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-10 bg-success">
+
+            <div className="row">
+              <div className="col-sm-4 border text-center bg-warning">
+              Mateusz
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              1
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              3
+              </div>
+              <div className="col-sm-4 border text-center bg-primary">
+              Michał, Paweł, Jacek, Stanisław
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-4 border text-center bg-light">
+              Mateusz
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              1
+              </div>
+              <div className="col-sm-1 border text-center bg-secondary text-light">
+              3
+              </div>
+              <div className="col-sm-4 border text-center bg-primary">
+              Michał, Paweł, Jacek, Stanisław
+              </div>
+            </div>
+
+
+          </div>
+
+          <div className="col-sm-2 border">
+          Sala
+          </div>
+        </div>
       </div>
     );
   }
