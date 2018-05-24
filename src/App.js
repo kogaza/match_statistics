c979c0b752d04be724ad67737fc2b646197901de
tenisistaa <krzychu_ten@o2.pl>
2018-05-24 22:49:23 +0200

    goals

--- src/App.js
+++ src/App.js
@@ -14,19 +14,34 @@ class App extends Component {
       ],
       players: [
         {id: 0, name: "Maciek"},
-        {id: 1, name: "Jan"}
+        {id: 1, name: "Jan"},
+        {id: 2, name: "Piotrek"},
+        {id: 3, name: "Jacek"},
+        {id: 4, name: "Mateusz"},
+        {id: 5, name: "Michał"},
+        {id: 6, name: "Paweł"}
       ],
       teams: [
         {id: 0, color: "white"},
         {id: 1, color: "orange"},
         {id: 2, color: "blue"}
+      ],
+      matchNumber: 1,
+      goals: [
+        {id: 0, matchNumber: 1, player: "Mateusz", team: "orange"},
+        {id: 1, matchNumber: 1, player: "Michał", team: "blue"},
+        {id: 2, matchNumber: 1, player: "Paweł", team: "blue"},
+        {id: 3, matchNumber: 1, player: "Jacek", team: "blue"},
+        {id: 4, matchNumber: 2, player: "Karol", team: "white"}
       ]
     }
   }
 
   addNewMatch = () => {
-    
-    console.log(this.state.players[0].name);
+    if(this.state.goals[2].matchNumber === 1){
+      console.log(this.state.goals[0].player);
+
+    }
   }
 
   render() {
@@ -40,7 +55,7 @@ class App extends Component {
             </div>
         <div className="row">
           <div className="col-sm-10 bg-success">
-            <NextRow row={this.state.results} />
+            <NextRow row={this.state.goals} />
           </div>
 
           <div className="col-sm-2 ">
